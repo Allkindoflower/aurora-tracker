aaf56815e3efcb6429127675703e0731cb5baca9
Uğur Baştuğ <bastugugur85@gmail.com>
2025-06-03 21:32:55 +0300

    Changed output to only print Aurora names for clarity

--- auroratracker.py
+++ auroratracker.py
@@ -7,11 +7,17 @@ api = 'http://api.auroras.live/v1/?type=locations'
 response = requests.get(api)
 
 data = response.json()
+names = []
 
 for key, aurora in data.items():
     if key == 'message':
         continue
     else:
-        print(f'Aurora name: {aurora['name']}||Country: {aurora['country']}'.strip())
+        names.append(aurora['name'])
+
+
+print('\n'.join(names))
+
+
 
 
